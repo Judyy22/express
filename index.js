5e82d0e76be41d56881bb9ce962db5f7c5a3be51
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2023-06-07 16:06:16 +0900

    revise

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const express = require("express");
 //express 사용
 const app = express();
 
-//포트번호
-const port = 8080;
+app.set("port", process.env.PORT || 8080); // 포트 설정
+app.set("host", process.env.HOST || "0.0.0.0"); // 아이피 설정
+
+// 루트 접속시 아이피 출력
+app.get("/", function (req, res) {
+    res.send("접속된 아이피: " + req.ip);
+});
+
+// 서버 동작중인 표시
+app.listen(app.get("port"), app.get("host"), () =>
+    console.log(
+        "Server is running on : " + app.get("host") + ":" + app.get("port")
+    )
+);
+
+app.post("/", function (req, res) {
+    res.send({ ip: req.ip });
+});
 
 //임시 데이터
 const courses = [
@@ -21,7 +37,7 @@ app.get("/api/courses", (req, res) => {
     res.send(courses);
 });
 
-// 해당하는 ID를 찾아서 Respon
+// 해당하는 ID를 찾아서 Response
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find((c) => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send(`ID was not found`);
@@ -37,8 +53,4 @@ app.post("/api/courses", (req, res) => {
     };
     courses.push(course);
     res.send(course);
-});
-
-app.listen(port, () => {
-    console.log(`서버가 실행됩니다. http://localhost:${port}`);
 });
